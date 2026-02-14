8695fa0206478836c272b041cf7f348270ed0cc1
sak96 <26397224+sak96@users.noreply.github.com>
2026-02-14 15:23:11 +0530

    Fix vue not working with base url

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import EditView from '../views/EditView.vue';
 import DeleteView from '../views/DeleteView.vue';
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
